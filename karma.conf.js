3ff3d9711fc6730519dd3bfed198ee54341cfe4b
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-03-17 19:06:33 +0000

    fix typo in last commit

--- karma.conf.js
+++ karma.conf.js
@@ -13,4 +13,8 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasm
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../../coverage'),
+      reports: ['html', 'lcovonly
