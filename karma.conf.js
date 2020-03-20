0fb6467abe93977695b00866feeab120cf6eace0
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-03-20 18:21:43 +0000

    icon changed

--- karma.conf.js
+++ karma.conf.js
@@ -17,4 +17,12 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+   
