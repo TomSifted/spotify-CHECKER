c29c4b256e2af6b475a282c60e4ae1c4da1230d3
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-04-11 14:07:58 +0000

    Improve grammar

---
+++ src/app/anchors/components/anchor-details/anchor-details.module.ts
@@ -0,0 +1,14 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnchorDetailsComponent } from './anchor-details.component';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CopyableTextModule } from '../../../shared/components';
+
+@NgModule({
+  declarations: [AnchorDetailsComponent],
+  imports: [CommonModule, MatCardModule, FlexLayoutModule, CopyableTextModule],
+  exports: [AnchorDetailsComponent],
+})
+export class AnchorDetailsModule {}
