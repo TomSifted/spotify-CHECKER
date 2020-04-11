31407a78dbc73f930562080e29b5db96fa23ea05
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-04-11 11:44:01 +0000

    move

--- src/app/anchors/anchors.module.ts
+++ src/app/anchors/anchors.module.ts
@@ -1,4 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared';
 import { AnchorsComponent } from './anchors.component';
-import { FileDropModule, AnchorDetailsModule } from './components
+import { FileDropModule, AnchorDetailsModule } from './components';
+
+@NgModule({
+  declarations: [AnchorsComponent],
+  imports: [SharedModule, FileDropModule, AnchorDetailsModule]
+})
+export class AnchorsModule {}
