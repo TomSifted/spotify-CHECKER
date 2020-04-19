ba7a194e980cb671d07081eaad62502e0f220fab
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-04-19 20:31:33 +0000

    Update to bundles-pom 10

--- src/app/anchors/components/file-drop/file-drop.module.ts
+++ src/app/anchors/components/file-drop/file-drop.module.ts
@@ -1,4 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileDropComponent } from './file-drop.component';
-import { MatIco
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxFileDropModule } from 'ngx-file-drop';
+
+@NgModule({
+  declarations: [FileDropCo
