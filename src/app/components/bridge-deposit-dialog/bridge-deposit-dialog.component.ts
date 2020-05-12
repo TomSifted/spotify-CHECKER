6b3c8359c214f594f83d1ca391aee22ac3a1fcf6
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-05-12 22:08:31 +0000

    temp push

--- src/app/components/bridge-deposit-dialog/bridge-deposit-dialog.component.ts
+++ src/app/components/bridge-deposit-dialog/bridge-deposit-dialog.component.ts
@@ -6,4 +6,11 @@ import { WalletService, BridgeService, toPromise } from '../../core';
   templateUrl: './bridge-deposit-dialog.component.html',
   styleUrls: ['./bridge-deposit-dialog.component.scss']
 })
-export class BridgeDepositDialogComp
+export class BridgeDepositDialogComponent implements OnInit {
+  step = 1;
+  bridgeAddress: string = '';
+  captcha: string = '';
+
+  constructor(private wallet: WalletService, private bridgeService: BridgeService) {}
+
+  ng
