17f1c293bac7e1a9095da1a25620c4d701466bd4
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-05-12 17:25:18 +0000

    Fix clippy warning

--- src/app/components/bridge-deposit-dialog/bridge-deposit-dialog.component.ts
+++ src/app/components/bridge-deposit-dialog/bridge-deposit-dialog.component.ts
@@ -2,4 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { WalletService, BridgeService, toPromise } from '../../core';
 
 @Component({
-  selector: 'lto-wallet-bridge-depo
+  selector: 'lto-wallet-bridge-deposit-dialog',
+  templateUrl: './bridge-deposit-dialog.component.html',
+  styleUrls: ['./bridge-deposit-dialog.component.scss']
+})
+export class BridgeDepositDialogComp
