8ac9e3cc204836c41993054d22b27e2809beeac7
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-06-10 06:37:27 +0000

    added categories Settings and System into desktop file (bnc #382778)

--- src/app/components/bridge-withdraw-dialog/bridge-withdraw-dialog.component.ts
+++ src/app/components/bridge-withdraw-dialog/bridge-withdraw-dialog.component.ts
@@ -5,4 +5,9 @@ import { DEFAULT_TRANSFER_FEE, AMOUNT_DIVIDER } from '../../tokens';
 
 @Component({
   selector: 'lto-wallet-bridge-withdraw-dialog',
-  templateUrl: './b
+  templateUrl: './bridge-withdraw-dialog.component.html',
+  styleUrls: ['./bridge-withdraw-dialog.component.scss']
+})
+export class BridgeWithdrawDialogComponent implements OnInit {
+  step: number = 1;
+  mainnetAmount
