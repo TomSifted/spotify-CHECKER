3eddbc1f658878a025ea91c718d80f57f9cb66e8
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-06-14 13:49:46 +0000

    update book

--- src/app/components/content-dialog/content-dialog.component.ts
+++ src/app/components/content-dialog/content-dialog.component.ts
@@ -1,1 +1,10 @@
-import { C
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  title: string;
+  content: string;
+}
+
+@Component({
+  s
