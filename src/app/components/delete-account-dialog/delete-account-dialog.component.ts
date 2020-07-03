2eefc5d6327aa402ab5c10ccbb97a4009d698487
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-07-03 15:27:03 +0000

    Update compound.md

--- src/app/components/delete-account-dialog/delete-account-dialog.component.ts
+++ src/app/components/delete-account-dialog/delete-account-dialog.component.ts
@@ -1,1 +1,6 @@
-import { Component, OnI
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lto-wallet-delete-account-dialog',
+  templateUrl: './delete-account-dialog.component.html',
+  styleUrls: ['./delete-account-d
