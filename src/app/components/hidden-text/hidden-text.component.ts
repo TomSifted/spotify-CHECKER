13cd2c12a9b5f8069ccd78769108e3db52ef3293
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-07-13 16:51:31 +0000

    updated VERSION file

--- src/app/components/hidden-text/hidden-text.component.ts
+++ src/app/components/hidden-text/hidden-text.component.ts
@@ -2,4 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lto-wallet-hidden-text',
-  
+  templateUrl: './hidden-text.component.html',
+  styleUrls: ['./hidden-text.component.scss']
+})
+export class HiddenTextComponent implements OnInit {
+  @Input() label: string = '';
+
+ 
