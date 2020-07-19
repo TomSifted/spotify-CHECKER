b3d19914cfb589b43f67fb2a514222deb8ea7cc4
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-07-19 17:19:02 +0000

    Added new rel The Musher

--- src/app/components/transaction-confirmation-dialog/transaction-confirmation-dialog.component.ts
+++ src/app/components/transaction-confirmation-dialog/transaction-confirmation-dialog.component.ts
@@ -1,1 +1,3 @@
-import { Comp
+import { Component, OnInit, Inject, ChangeDetectionStrategy } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { MAT_DIALOG_DATA } from '@angular/mat
