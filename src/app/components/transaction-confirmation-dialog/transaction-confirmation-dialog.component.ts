7601c36e3375981e5ec573d088a53e5515a8cebd
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-07-19 17:39:21 +0000

    Update adventure.js

--- src/app/components/transaction-confirmation-dialog/transaction-confirmation-dialog.component.ts
+++ src/app/components/transaction-confirmation-dialog/transaction-confirmation-dialog.component.ts
@@ -1,3 +1,12 @@
 import { Component, OnInit, Inject, ChangeDetectionStrategy } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
-import { MAT_DIALOG_DATA } from '@angular/mat
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface TransactionConfirmDialogData {
+  title?: string;
+  transactionData: TransactionDataField[];
+}
+
+interface TransactionDataField {
+  label: string;
+  value: st
