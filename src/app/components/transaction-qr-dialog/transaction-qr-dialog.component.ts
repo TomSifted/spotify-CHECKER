c8f931feb531f3da410d8a270fae323429c1ee08
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-08-29 06:58:39 +0000

    Update GUI.java

--- src/app/components/transaction-qr-dialog/transaction-qr-dialog.component.ts
+++ src/app/components/transaction-qr-dialog/transaction-qr-dialog.component.ts
@@ -1,2 +1,6 @@
 import { Component, OnInit, Inject, ChangeDetectionStrategy } from '@angular/core';
-import {
+import { DecimalPipe } from '@angular/common';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { WalletService } from '@app/core';
+
+export interf
