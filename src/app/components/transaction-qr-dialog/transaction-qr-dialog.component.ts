e78ea6f23fc6e7aa228b4d59279fa33db3fbefdc
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-08-29 14:20:11 +0000

    Update configurations

--- src/app/components/transaction-qr-dialog/transaction-qr-dialog.component.ts
+++ src/app/components/transaction-qr-dialog/transaction-qr-dialog.component.ts
@@ -3,4 +3,12 @@ import { DecimalPipe } from '@angular/common';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { WalletService } from '@app/core';
 
-export interf
+export interface TransactionQrDialogData {
+  title?: string;
+  tx: object;
+  transactionData: TransactionDataField[];
+}
+
+interface TransactionDataField {
+  label: string;
+  value: string |
