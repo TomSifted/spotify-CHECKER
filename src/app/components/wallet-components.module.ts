62570f80efbce51da979ebf6588785609fedfc5d
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-09-13 10:33:56 +0000

    add nicknames

--- src/app/components/wallet-components.module.ts
+++ src/app/components/wallet-components.module.ts
@@ -1,3 +1,7 @@
 import { NgModule } from '@angular/core';
 import { QRCodeModule } from 'angularx-qrcode';
-import { RecaptchaModule } from 'ng-recaptc
+import { RecaptchaModule } from 'ng-recaptcha';
+import { SharedModule } from '../shared';
+
+import { AmountInputComponent } from './amount-input/amount-input.component';
+import { BridgeDepositDialogComponent } from './br
