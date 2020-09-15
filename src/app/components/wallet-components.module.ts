6257dec9e7a044a5ee18efd5d60a9e9bba5894fe
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-09-15 06:16:03 +0000

    zram: fix invalid memory access

--- src/app/components/wallet-components.module.ts
+++ src/app/components/wallet-components.module.ts
@@ -5,4 +5,7 @@ import { SharedModule } from '../shared';
 
 import { AmountInputComponent } from './amount-input/amount-input.component';
 import { BridgeDepositDialogComponent } from './bridge-deposit-dialog/bridge-deposit-dialog.component';
-import { BridgeWithdrawDialogComponent } from './bridge-withdraw-dialog/bridge-withdraw-dialog.
+import { BridgeWithdrawDialogComponent } from './bridge-withdraw-dialog/bridge-withdraw-dialog.component';
+import { ContentDialogComponent } from './content-dialog';
+import { DeleteAccountDialogComponent } from './delete-account-dialog/delete-account-dialog.component';
+i
