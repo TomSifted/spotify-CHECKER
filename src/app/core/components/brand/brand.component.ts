b4ef2ccbe7f4b384b46e64c62f3d40b81cea88f1
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-10-26 11:37:49 +0000

    Updated menu

--- src/app/core/components/brand/brand.component.ts
+++ src/app/core/components/brand/brand.component.ts
@@ -1,1 +1,9 @@
-import { Component, OnInit } from
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lto-brand',
+  template: `
+    <a routerLink="/"> <img src="assets/LTO-LOGO.png" alt="" /> <strong>LTO</strong> Wallet </a>
+  `,
+  styles: [
+ 
