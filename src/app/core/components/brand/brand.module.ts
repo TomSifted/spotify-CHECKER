10e22f82c88665b46b6087bc14ff3c8b458ff4c9
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-11-08 14:48:18 +0000

    am: Use cat instead of echo to avoid DOS line-endings (fixes t4150)

--- src/app/core/components/brand/brand.module.ts
+++ src/app/core/components/brand/brand.module.ts
@@ -1,2 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angu
+import { RouterModule } from '@angular/router';
+import { BrandComponent } from './brand.component';
+
+@NgModule({
+  imports: [RouterModule.forChild([])],
+  declarations: [BrandComponent],
+  exports: [BrandComponent]
+})
+export class B
