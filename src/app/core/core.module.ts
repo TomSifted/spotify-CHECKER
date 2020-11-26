dd681b73df479108e65510434048dc1ecc997188
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-11-26 15:17:55 +0000

    0.5.18

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule } from '@angular/material/
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AppbarModule, SidenavModule } from './components';
+
+import {
+  PublicNode,
+  BridgeService,
+  EncoderService,
+  WalletService,
+  AuthService,
+  addresValidatorP
