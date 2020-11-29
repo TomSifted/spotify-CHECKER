7f4513eb39c9ebc00365429d6ec7bc0014d2db58
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-11-29 22:07:14 +0000

    src/Builder4/RooProjectSettings.bjs src/Builder4/RooProjectSettings.vala

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,3 +1,11 @@
 import { Injectable } from '@angular/core';
 import { tap, map } from 'rxjs/operators';
-import { Observable, c
+import { Observable, combineLatest } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+
+import { AuthService } from '../services';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth
