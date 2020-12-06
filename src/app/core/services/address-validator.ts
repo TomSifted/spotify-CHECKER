d43a4def6a834a670ca11a4b73a59db72b77b056
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-12-06 11:05:03 +0000

    Upgraded erlv8

--- src/app/core/services/address-validator.ts
+++ src/app/core/services/address-validator.ts
@@ -1,4 +1,8 @@
 import { LTO_NETWORK_BYTE, LTO_PUBLIC_API } from '../../tokens';
 import { ValidatorFn } from '@angular/forms';
 import { LTO } from 'lto-api';
-import { FactoryProvider, Injection
+import { FactoryProvider, InjectionToken } from '@angular/core';
+import { WalletService } from './wallet.service';
+import { WavesService } from './waves.service';
+
+export function addressValidatorFac
