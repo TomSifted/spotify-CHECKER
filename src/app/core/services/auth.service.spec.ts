6c5b0e282fded20a2dc53d93ea205126ebde3116
TomSifted <107317804+TomSifted@users.noreply.github.com>
2020-12-28 20:00:38 +0000

    add settings

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,2 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { LTO_MOBILE_AU
+import { LTO_MOBILE_AUTH, LTO_NETWORK_BYTE, LTO_PUBLIC_API } from '../../tokens';
+
+import { LedgerServiceMock } from './mocks';
+
+import { AuthService } from './auth.service';
+
+describe('co
