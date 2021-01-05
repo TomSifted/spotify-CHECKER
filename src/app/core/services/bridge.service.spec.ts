2dc2110cb872a13f3f4c85204e8ed42561c15967
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-01-05 14:38:47 +0000

    New translations ja-JP.yml (Catalan)

--- src/app/core/services/bridge.service.spec.ts
+++ src/app/core/services/bridge.service.spec.ts
@@ -1,1 +1,4 @@
-import { TestBed, inject } from '@angular/core/testi
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BridgeService, BridgeServiceImpl } from './bridge.service';
+import { LTO_BRIDGE_HOST, BRIDGE_ENAB
