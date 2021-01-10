d73fe5bb776a1061698fb784a87e93879dfb49be
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-01-10 21:27:17 +0000

    qseecom: Check the return value of "ion_phys"

--- src/app/core/services/bridge.service.spec.ts
+++ src/app/core/services/bridge.service.spec.ts
@@ -1,4 +1,9 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BridgeService, BridgeServiceImpl } from './bridge.service';
-import { LTO_BRIDGE_HOST, BRIDGE_ENAB
+import { LTO_BRIDGE_HOST, BRIDGE_ENABLED } from '@app/tokens';
+
+function configureTestingModule(bridgeEnabled: boolean) {
+  TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    p
