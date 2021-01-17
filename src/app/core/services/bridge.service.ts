079c3105bf6f560ae32d6cf709bea0992aa13382
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-01-17 21:43:37 +0000

    Bump is-my-json-valid from 2.15.0 to 2.20.0

--- src/app/core/services/bridge.service.ts
+++ src/app/core/services/bridge.service.ts
@@ -1,2 +1,5 @@
 import { Injectable, Inject, ClassProvider } from '@angular/core';
-import { HttpClient } from '@angular/
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { map, tap, shareReplay } from 'rxjs/operators';
+import { LTO_BRIDGE_HOST, BRIDGE_ENABLED } 
