fe46dc8655b254211cb0c02bb507f34a75d4f21c
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-01-17 22:32:57 +0000

    Update shop.py

--- src/app/core/services/bridge.service.ts
+++ src/app/core/services/bridge.service.ts
@@ -2,4 +2,13 @@ import { Injectable, Inject, ClassProvider } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { map, tap, shareReplay } from 'rxjs/operators';
-import { LTO_BRIDGE_HOST, BRIDGE_ENABLED } 
+import { LTO_BRIDGE_HOST, BRIDGE_ENABLED } from '../../tokens';
+
+export type TokenType = 'LTO' | 'LTO20' | 'WAVES' | 'BINANCE';
+
+interface BridgeCache {
+  deposit: {
+    [address: string]: string;
+  };
+
+  
