125d78113b1a858ab5c13ef9b9dc17be3f16aade
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-04-11 06:00:54 +0000

    Release v1.11.0

--- src/app/core/services/encoder.service.ts
+++ src/app/core/services/encoder.service.ts
@@ -4,4 +4,9 @@ import { Buffer } from 'buffer';
 @Injectable()
 export class EncoderService {
   private alphabet: string;
-  private alphabetMap: { [key: string]: 
+  private alphabetMap: { [key: string]: number };
+
+  constructor() {
+    this.alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+    this.alphabetMap = this.alphabet.split('').reduce(
+      
