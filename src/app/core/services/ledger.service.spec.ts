563b98b7a36b67e4c685c8871b9890be0a82538b
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-05-24 19:03:02 +0000

    Increment version for release

--- src/app/core/services/ledger.service.spec.ts
+++ src/app/core/services/ledger.service.spec.ts
@@ -1,1 +1,5 @@
-import { TestBed } from '@angular/core/testing'
+import { TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { LedgerService } from './ledger.service';
+import { LTO_NETWORK_BYTE, LTO_PUBLIC_API } from '@app/tokens';
