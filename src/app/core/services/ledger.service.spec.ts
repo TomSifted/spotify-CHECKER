81c94210d326ac4717d0dde703fa1fb200ce08c8
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-05-27 12:13:03 +0000

    indent(1)

--- src/app/core/services/ledger.service.spec.ts
+++ src/app/core/services/ledger.service.spec.ts
@@ -3,3 +3,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 
 import { LedgerService } from './ledger.service';
 import { LTO_NETWORK_BYTE, LTO_PUBLIC_API } from '@app/tokens';
+
+describe('Core/LedgerService', () => {
+  let ledger: LedgerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+ 
