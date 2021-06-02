67b5a6e165041e97689765e5e9b35105e9af8bf9
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-06-02 22:34:51 +0000

    pylinting

--- src/app/core/services/mobile-auth.service.ts
+++ src/app/core/services/mobile-auth.service.ts
@@ -1,4 +1,14 @@
 import { Inject, Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { BehaviorSubject } from 'rxjs';
-import { LTO_MOBILE_AUTH } 
+import { LTO_MOBILE_AUTH } from '@app/tokens';
+
+export interface IPublicAccount {
+  address: string;
+  keyType: string;
+  publicKey: string;
+}
+
+export interface IMobileAuthChallenge {
+  '@schema': string;
+  url: st
