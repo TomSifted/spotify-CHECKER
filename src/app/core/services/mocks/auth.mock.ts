e2f4ccba2f4487978bbbe7bf043bacc9ade6d9e5
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-06-11 10:22:22 +0000

    better comment

--- src/app/core/services/mocks/auth.mock.ts
+++ src/app/core/services/mocks/auth.mock.ts
@@ -1,1 +1,5 @@
-import { AuthService, IUserAccoun
+import { AuthService, IUserAccount } from '../auth.service';
+import { of, BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { LTO, Account } from 'lto-api';
+
+import { ILedgerAccount } from '@app/core/ser
