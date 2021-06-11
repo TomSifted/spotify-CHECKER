6f25c69d1f940efb1c1ee22835ebd05e8193b549
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-06-11 19:01:49 +0000

    update what's new

--- src/app/core/services/mocks/auth.mock.ts
+++ src/app/core/services/mocks/auth.mock.ts
@@ -2,4 +2,10 @@ import { AuthService, IUserAccount } from '../auth.service';
 import { of, BehaviorSubject, Observable, combineLatest } from 'rxjs';
 import { LTO, Account } from 'lto-api';
 
-import { ILedgerAccount } from '@app/core/ser
+import { ILedgerAccount } from '@app/core/services/ledger.service';
+import { map } from 'rxjs/operators';
+
+export class AuthServiceMock implements AuthService {
+  static provider = {
+    provide: AuthService,
+    useClass: AuthServiceM
