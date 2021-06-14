bcc10b1c300d66dbe6ed5675748605b1c7bd5dcc
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-06-14 13:35:09 +0000

    Update AvaloniaRenderer.cs

--- src/app/core/services/mocks/bridge.service.mock.ts
+++ src/app/core/services/mocks/bridge.service.mock.ts
@@ -1,1 +1,7 @@
-import { BridgeService } from '
+import { BridgeService } from '../bridge.service';
+import { of } from 'rxjs';
+
+export class BridgeServiceMock implements BridgeService {
+  static provider = {
+    provide: BridgeService,
+    useClass: BridgeServi
