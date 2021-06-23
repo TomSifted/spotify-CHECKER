88e44f901d22c5a801cccd5086cc4d2157e29c1b
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-06-23 19:14:06 +0000

    build(deps-dev): bump @typescript-eslint/eslint-plugin

--- src/app/core/services/scripts.service.ts
+++ src/app/core/services/scripts.service.ts
@@ -1,3 +1,13 @@
 import { Observable } from 'rxjs';
 
-export interface PredefinedScrip
+export interface PredefinedScript {
+  label: string;
+  value: string;
+}
+
+/**
+ * Smart scripts service interface.
+ *
+ * You can find implemetation in : ./scripts.service.impl.ts
+ */
+export abstract class ScriptsService 
