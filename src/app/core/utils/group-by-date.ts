9cf8decb903c3e51d05450ef8569aa1cabd3b5a9
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-06-28 18:37:39 +0000

    Updated build number

--- src/app/core/utils/group-by-date.ts
+++ src/app/core/utils/group-by-date.ts
@@ -1,4 +1,12 @@
 import * as moment from 'moment';
 
 export interface TransactionsGroup {
-  date: stri
+  date: string;
+  transactions: any[];
+}
+
+/**
+ * Groups transactions by same date
+ * @param transactions - array of transactions
+ */
+export function groupByDate(transactions: any[]): TransactionsGroup
