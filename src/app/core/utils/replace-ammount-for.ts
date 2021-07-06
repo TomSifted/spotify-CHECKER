b245f81ba066ee6a747d46f7f3044d048f662e41
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-07-06 17:54:34 +0000

    catch up

--- src/app/core/utils/replace-ammount-for.ts
+++ src/app/core/utils/replace-ammount-for.ts
@@ -3,4 +3,14 @@
  * @param address - address for which ammount will be calculated
  */
 export function replaceAmountFor(address: string) {
-  return function(transactions: any[]) 
+  return function(transactions: any[]) {
+    return transactions.map((transaction: any) => {
+      return {
+        ...transaction,
+        amount: transactionAmount(transaction, address)
+      };
+    });
+  };
+}
+
+functi
