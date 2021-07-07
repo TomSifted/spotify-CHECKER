c32bc9391edb542027b5c9a4110f6d533d40ec7d
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-07-07 20:03:07 +0000

    + ‘tidal/spicule/.#three.tidal’

--- src/app/create-account/components/seed/seed.component.ts
+++ src/app/create-account/components/seed/seed.component.ts
@@ -3,4 +3,11 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'lto-wallet-seed',
   template: `
-    <span class="word" *ngFor="let word of words; last as isLast">{{ wor
+    <span class="word" *ngFor="let word of words; last as isLast">{{ word }}<ng-container *ngIf="!isLast">&nbsp;</ng-container></span>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        padding: 16px;
+        border: 1p
