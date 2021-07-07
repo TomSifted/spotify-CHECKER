e06c44a47e69e1c36433616f41657bd27ca8d1b0
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-07-07 09:30:50 +0000

    Update lodash

--- src/app/core/utils/transactions-filter.ts
+++ src/app/core/utils/transactions-filter.ts
@@ -1,4 +1,8 @@
 import { TransactionTypes } from '../transaction-types';
 /**
  * Factory of transaction filters.
- * Returns function which accepts transactions array and retu
+ * Returns function which accepts transactions array and returns filtered by provided types numbers
+ * @param types - transaction types to filter
+ */
+export function transactionsFilter(...types: TransactionTypes[]) {
+  return function(transactions:
