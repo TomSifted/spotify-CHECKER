5633cefd3e6260bce9c06297ebf3026f71f1ffde
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-07-07 11:48:11 +0000

    2.1.12

--- src/app/create-account/components/seed/seed.component.spec.ts
+++ src/app/create-account/components/seed/seed.component.spec.ts
@@ -1,1 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SeedComponent } from './seed.component';
+
+describe('create-account/SeedComponent', () => {
+  let component: SeedComponent;
+  let fixture: ComponentFixture<SeedComponent>;
+
+  beforeE
