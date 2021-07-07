31836a5377bb7d9f39b45dac5adaf9c6897f89c2
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-07-07 18:40:14 +0000

    One more.

--- src/app/create-account/components/seed/seed.component.ts
+++ src/app/create-account/components/seed/seed.component.ts
@@ -1,1 +1,6 @@
-import 
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'lto-wallet-seed',
+  template: `
+    <span class="word" *ngFor="let word of words; last as isLast">{{ wor
