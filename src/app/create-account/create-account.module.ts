bc5e66a769385f9cbf67b7cde8c8fa92c1a20225
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-08-04 23:03:53 +0000

    added videos

--- src/app/create-account/create-account.module.ts
+++ src/app/create-account/create-account.module.ts
@@ -1,3 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared';
-import
+import { RouterModule } from '@angular/router';
+import { CreateAccountComponent } from './create-account.component';
+import { SeedComponent } from './components/seed/seed.component';
+
+@NgMod
