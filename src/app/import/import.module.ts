87a70a48a3601f618c84234a6581719d00fc2762
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-08-17 13:49:02 +0000

    v1.5.8

--- src/app/import/import.module.ts
+++ src/app/import/import.module.ts
@@ -1,3 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import 
