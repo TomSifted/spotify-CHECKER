2238ec1ddea7e42b9991fa8e8b556071ac7aabac
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-04 06:42:47 +0000

    Fiddling with CircleCI config file

---
+++ src/app/leasing/leasing.component.ts
@@ -0,0 +1,74 @@
+
+import { Component, OnInit, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { WalletService, TransactionTypes, transactionsFilter, toPromise } from '../core';
+import { StartLeaseModal } from '../modals';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TransactionConfirmDialog } from '../components/transaction-confirmation-dialog';
+import { AMOUNT_DIVIDER } from '@app/tokens';
+import { TransactionQrDialog } from '@app/components/transaction-qr-dialog';
+
+@Component({
+  selector: 'lto-leasing',
+  templateUrl: './leasing.component.html',
+  styleUrls: ['./leasing.component.scss'],
+})
+export class LeasingComponent implements OnInit {
+  transactions$: Observable<any[]>;
+  address$: Observable<string>;
+
+  selectedTransaction: any = null;
+
+  get detailsOpened(): boolean {
+    return !!this.selectedTransaction;
+  }
+
+  constructor(
+    private confirmDialog: TransactionConfirmDialog,
+    private qrDialog: TransactionQrDialog,
+    private wallet: WalletService,
+    private startLeaseModal: StartLeaseModal,
+    private snackbar: MatSnackBar,
+    @Inject(AMOUNT_DIVIDER) private AMOUNT_DIVIDER: number
+  ) {
+    this.address$ = wallet.address$;
+    this.transactions$ = wallet.leasingTransactions$.pipe(
+      map((transactions) => {
+        // Now we need to mark canceling transactions
+        // First - get all canceling transactions
+        const canceling = transactionsFilter(TransactionTypes.CANCEL_LEASING)(transactions).map(
+          (transaction) => transaction.lease.id
+        );
+
+        // Get our leasing transactions
+        const leasing = transactionsFilter(TransactionTypes.LEASING)(transactions);
+
+        // Now we need to go through active leasing and if it is in process of canceling
+        // mark it
+        return leasing.map((transaction) => {
+          return {
+            ...transaction,
+            isCanceling: canceling.indexOf(transaction.id) !== -1,
+          };
+        });
+      })
+    );
+  }
+
+  ngOnInit() {}
+
+  select(transaction: any) {
+    this.selectedTransaction = transaction;
+  }
+
+  async startLease() {
+    const balance = await toPromise(this.wallet.balance$);
+    const leaseData = await this.startLeaseModal.show(balance.available);
+    if (!leaseData) {
+      return;
+    }
+    try {
+      if (leaseData !== true) { // If leaseData is true it means the tx has been sent using a QR.
+        await this.wallet.lease({ ...leaseData });
+      }
