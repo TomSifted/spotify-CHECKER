a9dc7e387d1d7eb7a3cf97f42fc6abb10e9e623e
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-07 21:03:21 +0000

    configure.ac

--- src/app/modals/create-script/create-script.component.ts
+++ src/app/modals/create-script/create-script.component.ts
@@ -1,2 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } fr
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
+import { ScriptsService, PredefinedScript } from '../../core';
+import { Observab
