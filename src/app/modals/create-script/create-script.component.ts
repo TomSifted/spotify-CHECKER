9bb085ec1ed139a51a1ebffbc08b9022e7a866ba
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-09 17:20:43 +0000

    restruct

--- src/app/modals/create-script/create-script.component.ts
+++ src/app/modals/create-script/create-script.component.ts
@@ -2,4 +2,12 @@ import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl } from '@angular/forms';
 import { ScriptsService, PredefinedScript } from '../../core';
-import { Observab
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'lto-create-script',
+  templateUrl: './create-script.component.html',
+  styleUrls: ['./create-script.component.scss'],
+})
+export class CreateScriptComponent {
+  scri
