2205e647f65f99c67529afc7623924a243115b9d
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-11 16:15:55 +0000

    aureooms-js-* -> @aureooms/js-*

--- src/app/modals/create-script/create-script.module.ts
+++ src/app/modals/create-script/create-script.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModul
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CreateScriptComponent } from './create-script.component';
+import { CreateScriptModal } from './create-script-modal';
+import { MatDialogModule } from '@ang
