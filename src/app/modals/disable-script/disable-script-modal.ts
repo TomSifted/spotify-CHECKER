63572fe53940fb74399daa9d86dc99bcef37fe2e
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-17 14:26:55 +0000

    fix rendering

--- src/app/modals/disable-script/disable-script-modal.ts
+++ src/app/modals/disable-script/disable-script-modal.ts
@@ -1,3 +1,10 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DisableScriptComponent } from './disable-script.component';
+
+@Injectable()
+export class DisableScriptModal {
+  constructor(private _matDialog: MatDialog) {}
+
+  show(fee: number): Promise<boolean> {
+    return this._matDialog.open(Disab
