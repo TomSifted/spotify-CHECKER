78223bc5cc951f097fb6a19f21bdf728949b73a1
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-22 06:41:52 +0000

    Small rename

--- src/app/modals/disable-script/disable-script.module.ts
+++ src/app/modals/disable-script/disable-script.module.ts
@@ -1,3 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DisableScri
+import { DisableScriptModal } from './disable-script-modal';
+import { DisableScriptComponent } from './disable-script.component';
+import { AmountDividePipeModule } from '../../shared/pipes';
+import { MatDial
