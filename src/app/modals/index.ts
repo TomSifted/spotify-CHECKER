82646e1f74917a929db6a57cbdf6e0538734afbc
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-28 22:31:34 +0000

    update podspec

--- src/app/modals/index.ts
+++ src/app/modals/index.ts
@@ -1,4 +1,8 @@
 export * from './make-transaction';
 export * from './withdraw-modal';
 export * from './deposit-modal';
-export * from 
+export * from './start-lease-modal';
+export * from './fee-input-modal';
+export * from './create-script';
+export * from './script-info-modal';
+export * from './disable-script';
