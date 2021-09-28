ae30ed7190eff5c1cbfb94bb9bbe46d3422c60bf
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-09-28 14:45:23 +0000

    more names

--- src/app/modals/fee-input-modal/fee-input-modal.module.ts
+++ src/app/modals/fee-input-modal/fee-input-modal.module.ts
@@ -2,4 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeeInputModalComponent } from './fee-input-modal.component';
 import { FormsModule } from '@angular/forms';
-import {
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/butt
