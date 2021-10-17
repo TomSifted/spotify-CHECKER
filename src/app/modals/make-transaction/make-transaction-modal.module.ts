74bf48f18dfe3b01a371c07099eb33d08905ef66
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-17 18:46:26 +0000

    Add some screenshots

--- src/app/modals/make-transaction/make-transaction-modal.module.ts
+++ src/app/modals/make-transaction/make-transaction-modal.module.ts
@@ -7,4 +7,16 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MakeTransactionComponent } from './make-transaction.component';
 import { MakeTransactionModal } from './make-transaction-modal';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MakeTransactionTransfersComponent } from './components/make-transaction-transfers/make-transaction-transfers.compon
+import { MakeTransactionTransfersComponent } from './components/make-transaction-transfers/make-transaction-transfers.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
+  ],
+  declarations: [
+    MakeTransactionComponent,
+    MakeTransactionTr
