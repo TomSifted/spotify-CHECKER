6adda4f358347da904196989f6c4c2b45be7ec50
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-17 14:59:59 +0000

    Minor doc fixes

--- src/app/modals/make-transaction/make-transaction-modal.module.ts
+++ src/app/modals/make-transaction/make-transaction-modal.module.ts
@@ -6,4 +6,5 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { MakeTransactionComponent } from './make-transaction.component';
 import { MakeTransactionModal } from './make-transaction-modal';
-import { MatProgressSpinnerModule } from '
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MakeTransactionTransfersComponent } from './components/make-transaction-transfers/make-transaction-transfers.compon
