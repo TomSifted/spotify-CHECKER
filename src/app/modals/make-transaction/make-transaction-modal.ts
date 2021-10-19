47c0d6b10f10d7fafbedc6a72aa5f1af8c0bafef
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-19 16:39:02 +0000

    lib/scatterlist.c: don't flush_kernel_dcache_page on slab page

--- src/app/modals/make-transaction/make-transaction-modal.ts
+++ src/app/modals/make-transaction/make-transaction-modal.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MakeTransactionComponent } from './make-transaction.component';
-import { toPromise } from '../../c
+import { toPromise } from '../../core';
+
+@Injectable()
+export class MakeTransactionModal {
+  constructor(private _dialog: MatDialog) {}
+
+  show(): Promise<void> {
+    const dialog = this._dialog.open(Mak
