29ed29bcf2bd975bf3975f97dab59794adce52c0
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-19 15:51:02 +0000

    Release 0.16.1

--- src/app/modals/make-transaction/make-transaction-modal.ts
+++ src/app/modals/make-transaction/make-transaction-modal.ts
@@ -1,1 +1,4 @@
-import { Injectable } from 
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MakeTransactionComponent } from './make-transaction.component';
+import { toPromise } from '../../c
