470534275b893ce6f0b9815194aff339b7be2fd4
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-25 21:53:32 +0000

    be more verbose

--- src/app/modals/mobile-auth-modal/mobile-auth-modal.component.ts
+++ src/app/modals/mobile-auth-modal/mobile-auth-modal.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { IMobileAuthChallenge, MobileAuthService } from '../../core';
-impo
+import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'lto-mobile-auth-modal',
+  t
