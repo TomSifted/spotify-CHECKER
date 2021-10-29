038ba79c7ca198fc0b80c6e408cedc7ff0ccaab6
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-29 12:06:16 +0000

    resolved merge conflicts

--- src/app/modals/modals.module.ts
+++ src/app/modals/modals.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MakeTransactionModalModule } from './make-transaction';
-import { WithdrawModalModule } from './wit
+import { WithdrawModalModule } from './withdraw-modal';
+import { DepositModalModule } from './deposit-modal';
+import { StartLeaseModalModule } from './start-lease-modal';
+import { FeeInputModalModule } from './fee-inp
