c9d3d5fb2b2d8dad0a9f63f221ab7d0fc2f793e9
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-10-30 14:45:45 +0000

    cover

--- src/app/modals/modals.module.ts
+++ src/app/modals/modals.module.ts
@@ -4,4 +4,7 @@ import { MakeTransactionModalModule } from './make-transaction';
 import { WithdrawModalModule } from './withdraw-modal';
 import { DepositModalModule } from './deposit-modal';
 import { StartLeaseModalModule } from './start-lease-modal';
-import { FeeInputModalModule } from './fee-inp
+import { FeeInputModalModule } from './fee-input-modal';
+import { CreateScriptModule } from './create-script';
+import { ScriptInfoModalModule } from './script-info-modal';
+import { DisableScriptModule } from './disable-script';
