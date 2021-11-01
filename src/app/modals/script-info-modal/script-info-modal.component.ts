7fc6de172abb3d306f53ff116ba5613a3717f790
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-01 10:46:11 +0000

    core

--- src/app/modals/script-info-modal/script-info-modal.component.ts
+++ src/app/modals/script-info-modal/script-info-modal.component.ts
@@ -1,1 +1,7 @@
-import { Component, OnInit } from '
+import { Component, OnInit } from '@angular/core';
+import { ScriptsService } from '../../core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'lto-wallet-script-info-modal',
+  templateUrl: './sc
