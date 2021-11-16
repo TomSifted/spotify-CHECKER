d66f744cfe6dae33430b1958b43b482b082db0c0
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-16 10:45:38 +0000

    release 0.0.3

--- src/app/modals/start-lease-modal/start-lease-modal.ts
+++ src/app/modals/start-lease-modal/start-lease-modal.ts
@@ -1,3 +1,9 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { StartLeaseModalComponent, LeaseData } from './sta
+import { StartLeaseModalComponent, LeaseData } from './start-lease-modal.component';
+
+@Injectable()
+export class StartLeaseModal {
+  constructor(private dialog: MatDialog) { }
+
+  show(balance: number): Promise<Lease
