7ee14e9ee416433bfcf1ee74083db443207cd99d
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-20 07:46:37 +0000

    vimrc

--- src/app/modals/withdraw-modal/withdraw-modal.component.ts
+++ src/app/modals/withdraw-modal/withdraw-modal.component.ts
@@ -6,4 +6,9 @@ import { WalletService, IBalance } from '../../core';
 
 @Component({
   selector: 'lto-withdraw-modal',
-  templateUrl: './withdraw
+  templateUrl: './withdraw-modal.component.html',
+  styleUrls: ['./withdraw-modal.component.scss'],
+})
+export class WithdrawModalComponent implements OnInit {
+  withdrawForm: FormGroup;
+  balance$: Observable<IB
