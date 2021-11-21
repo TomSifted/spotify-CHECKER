ec505307add31b853d533a836382818b7ce07af5
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-21 13:26:22 +0000

    Update templates.

--- src/app/modals/withdraw-modal/withdraw-modal.ts
+++ src/app/modals/withdraw-modal/withdraw-modal.ts
@@ -1,2 +1,8 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { WithdrawModalComponent } from './withdraw-modal.component';
+import { toPromise } from '../../core';
+
+@Injectable()
+export class WithdrawModal {
+  const
