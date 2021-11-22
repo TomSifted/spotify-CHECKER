abf819fc358e10f3d76cee4102058053b2809d12
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-22 22:17:10 +0000

    drm/radeon: avoid leaking edid data

--- src/app/pages/bridge/bridge-routing.module.ts
+++ src/app/pages/bridge/bridge-routing.module.ts
@@ -1,2 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, Rou
+import { Routes, RouterModule } from '@angular/router';
+import { BridgeComponent } from './bridge.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BridgeComponent
+  }
+];
+
+@N
