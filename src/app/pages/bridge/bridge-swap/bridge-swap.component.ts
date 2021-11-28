abcfe5aabb3e39777de6642834b1fc79584ea6ed
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-28 19:55:36 +0000

    v0.40

--- src/app/pages/bridge/bridge-swap/bridge-swap.component.ts
+++ src/app/pages/bridge/bridge-swap/bridge-swap.component.ts
@@ -1,2 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dial
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { SwapType } from './swap-type';
+import { MakeTransactionModal } from '../../../modals';
+
+@Component({
+  selector: 'lto-wallet-brid
