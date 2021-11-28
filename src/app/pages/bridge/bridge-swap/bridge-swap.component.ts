1b8556e31f83a7b35b6640f724b129f21ee3b166
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-11-28 20:59:13 +0000

    Update template

--- src/app/pages/bridge/bridge-swap/bridge-swap.component.ts
+++ src/app/pages/bridge/bridge-swap/bridge-swap.component.ts
@@ -5,4 +5,10 @@ import { SwapType } from './swap-type';
 import { MakeTransactionModal } from '../../../modals';
 
 @Component({
-  selector: 'lto-wallet-brid
+  selector: 'lto-wallet-bridge-swap',
+  templateUrl: './bridge-swap.component.html',
+  styleUrls: ['./bridge-swap.component.scss'],
+})
+export class BridgeSwapComponent implements OnInit {
+  step = 2;
+ 
