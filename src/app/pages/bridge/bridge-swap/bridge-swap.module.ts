c9d2f187cdca5fbc9b8f1c73998100af55ed15ca
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-12-01 06:18:18 +0000

    Added Czech language

--- src/app/pages/bridge/bridge-swap/bridge-swap.module.ts
+++ src/app/pages/bridge/bridge-swap/bridge-swap.module.ts
@@ -1,3 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/form
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { QRCodeModule } from 'angularx-qrcode';
+import { BridgeSwapComponent } from './bridge-swap.component';
+import { SwapTypeComp
