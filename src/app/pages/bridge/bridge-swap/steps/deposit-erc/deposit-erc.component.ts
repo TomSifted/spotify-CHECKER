7057279dd6b873fd3d5a8f3f0b186a62f5521227
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-12-05 17:02:05 +0000

    update da translation

--- src/app/pages/bridge/bridge-swap/steps/deposit-erc/deposit-erc.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/deposit-erc/deposit-erc.component.ts
@@ -1,3 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BridgeService, WalletService } from '../../../../../core';
-import { switchMap }
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { SwapType } from '../../swap-type';
+import { AbstractControl, FormControl, FormGroup, Valid
