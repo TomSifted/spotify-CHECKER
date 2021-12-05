cf9022a353c27eba10c6b969f7d838545a8ea428
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-12-05 19:10:59 +0000

    debugging edit

--- src/app/pages/bridge/bridge-swap/steps/deposit-erc/deposit-erc.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/deposit-erc/deposit-erc.component.ts
@@ -3,4 +3,9 @@ import { BridgeService, WalletService } from '../../../../../core';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { SwapType } from '../../swap-type';
-import { AbstractControl, FormControl, FormGroup, Valid
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import * as bech32 from 'bech32';
+
+@Component({
+  selector: 'lto-wallet-deposit-erc',
+  templateUrl: './deposit-erc.component.ht
