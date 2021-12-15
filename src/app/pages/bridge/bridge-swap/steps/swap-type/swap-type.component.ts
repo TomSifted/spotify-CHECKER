26fd551cb6d75042dc13e7207d0c8bd80977993a
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-12-15 09:57:12 +0000

    Update openjdk

--- src/app/pages/bridge/bridge-swap/steps/swap-type/swap-type.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/swap-type/swap-type.component.ts
@@ -6,4 +6,10 @@ import { SwapType } from '../../swap-type';
 @Component({
   selector: 'lto-wallet-swap-type',
   templateUrl: './swap-type.component.html',
-  styleUrls: ['./swap-type.component.s
+  styleUrls: ['./swap-type.component.scss']
+})
+export class SwapTypeComponent {
+  @Output() selectType = new EventEmitter<SwapType>();
+
+  selectedType: SwapType | null = null;
+  SwapType
