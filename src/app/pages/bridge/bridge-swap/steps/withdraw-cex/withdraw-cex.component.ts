5ac07bdbf6063e3401dbc87cb7d69600613d1f24
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-12-16 16:09:30 +0000

    chore(package): update should to version 9.0.0

--- src/app/pages/bridge/bridge-swap/steps/withdraw-cex/withdraw-cex.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/withdraw-cex/withdraw-cex.component.ts
@@ -1,1 +1,5 @@
-import { Comp
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SwapType } from '../../swap-type';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import
