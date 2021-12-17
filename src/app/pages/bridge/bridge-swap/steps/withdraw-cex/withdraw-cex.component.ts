7005f64a90dc665845f27404fed5fdb9bc98f64d
TomSifted <107317804+TomSifted@users.noreply.github.com>
2021-12-17 06:11:32 +0000

    -Wmissing-include-dirs

--- src/app/pages/bridge/bridge-swap/steps/withdraw-cex/withdraw-cex.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/withdraw-cex/withdraw-cex.component.ts
@@ -2,4 +2,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SwapType } from '../../swap-type';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import
+import { BridgeService } from '../../../../../core';
+
+@Component({
+  selector: 'lto-wallet-withdraw-cex',
+  templateUrl: './withdraw-cex.component.html',
+  styleUrls: ['./withdraw-cex.component.scss']
+})
+exp
