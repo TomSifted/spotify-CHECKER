c2abd196fa580d8d9731620efc707fb0ba310e0f
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-03-19 20:18:47 +0000

    New translations translation.json (French)

--- src/app/pages/bridge/bridge-swap/steps/withdraw-form/withdraw-form.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/withdraw-form/withdraw-form.component.ts
@@ -1,2 +1,4 @@
 import { Component, OnInit, Output, EventEmitter, Inject, OnDestroy, Input } from '@angular/core';
-import { FormGroup, FormControl, Validators, Abst
+import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { Observable, combineLatest, ReplaySubject, Subscription } from 'rxjs';
+import { BridgeService, WalletService, eth
