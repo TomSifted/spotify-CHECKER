cebb6bc7e6ff71bc718407ef342bb1838a430be1
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-03-20 10:05:09 +0000

    License updates

--- src/app/pages/bridge/bridge-swap/steps/withdraw-form/withdraw-form.component.ts
+++ src/app/pages/bridge/bridge-swap/steps/withdraw-form/withdraw-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Inject, OnDestroy, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { Observable, combineLatest, ReplaySubject, Subscription } from 'rxjs';
-import { BridgeService, WalletService, eth
+import { BridgeService, WalletService, etheriumAddressValidator } from '../../../../../core';
+import { DEFAULT_TRANSFER_FEE } from '../../../../../tokens';
+import { map, withLatestFrom, take } from 'rxjs/operators';
+import { SwapType } from '../../sw
