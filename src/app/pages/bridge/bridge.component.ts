262eec74cb2fcde220a1048a116f9c1d2883bde5
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-05-15 21:53:49 +0000

    Delete groupmanager.lua

--- src/app/pages/bridge/bridge.component.ts
+++ src/app/pages/bridge/bridge.component.ts
@@ -1,1 +1,7 @@
-import { Component, OnInit } from 
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BridgeSwapComponent } from './bridge-swap';
+
+@Component({
+  selector: 'lto-wallet-bridge',
+  templateUrl:
