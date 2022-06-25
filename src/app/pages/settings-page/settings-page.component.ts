3c51652da576cde0df85dba5fd4dfecf98a34d37
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-06-25 06:22:33 +0000

    Some styling

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { Account } from 'lto-api';
-im
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { LedgerService } from '@app/core/services';
+import { CreateScriptModal, ScriptInfoModal,
