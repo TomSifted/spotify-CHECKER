750326f4c9ea66c5ec42da879ac0f664c5222b86
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-08-21 22:21:08 +0000

    UPDATE CHANGES_NEXT_RELEASE

--- src/app/shared/components/content-section/content-section.module.ts
+++ src/app/shared/components/content-section/content-section.module.ts
@@ -1,1 +1,7 @@
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContentSectionComponent } from './content-section.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declaratio
