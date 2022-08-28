dd07a5274a5e2e2a7c29a8bbd40cfd42ff62f6da
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-08-28 17:40:23 +0000

    Fix rubocop warning

--- src/app/shared/components/copyable-text/copyable-text.component.ts
+++ src/app/shared/components/copyable-text/copyable-text.component.ts
@@ -1,2 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MatSnackBar } from 
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'lto-copyable-text',
+  templateUrl: './copyable-text.component.html',
+  styleUrls: ['./copyable-text.comp
