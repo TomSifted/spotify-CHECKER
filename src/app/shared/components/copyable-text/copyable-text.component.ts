03d792dcb276515b9d3dea3c1f557c03b311a9b5
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-08-30 09:22:28 +0000

    hrtimer: Set expiry time before switch_hrtimer_base()

--- src/app/shared/components/copyable-text/copyable-text.component.ts
+++ src/app/shared/components/copyable-text/copyable-text.component.ts
@@ -4,4 +4,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'lto-copyable-text',
   templateUrl: './copyable-text.component.html',
-  styleUrls: ['./copyable-text.comp
+  styleUrls: ['./copyable-text.component.scss'],
+})
+export class CopyableTextComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() label: string = '';
+
+  constructor(private snackbar: MatSnackBar) {}
+
+  ngOnInit() {}
+
+  
