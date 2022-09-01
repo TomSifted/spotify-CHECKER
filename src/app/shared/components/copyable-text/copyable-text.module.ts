2645c3fb58cdda8ebe4e1fc58a0d92d6a4a999f3
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-09-01 22:55:44 +0000

    fix deps

--- src/app/shared/components/copyable-text/copyable-text.module.ts
+++ src/app/shared/components/copyable-text/copyable-text.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CopyableTextComponent } from './copyable-text.component';
-import { FlexLayoutModu
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { MatTooltipModule }
