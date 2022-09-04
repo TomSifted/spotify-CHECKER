514ddb5338bad08aa09a0caf1fc4821e71cbd1ff
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-09-04 08:01:56 +0000

    -x

--- src/app/shared/components/copyable-text/copyable-text.module.ts
+++ src/app/shared/components/copyable-text/copyable-text.module.ts
@@ -4,4 +4,8 @@ import { CopyableTextComponent } from './copyable-text.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRippleModule } from '@angular/material/core';
-import { MatTooltipModule }
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [CopyableTextComponent],
+  imports: [CommonModule, FlexLayoutModule, MatIconModule, MatRippleModule, 
