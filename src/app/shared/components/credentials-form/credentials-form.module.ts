d71f5cfc4369f38417e9d09f39eb459fa359f274
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-09-05 19:16:24 +0000

    80

--- src/app/shared/components/credentials-form/credentials-form.module.ts
+++ src/app/shared/components/credentials-form/credentials-form.module.ts
@@ -2,4 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrendetialsFormComponent } from './credentials-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: 
