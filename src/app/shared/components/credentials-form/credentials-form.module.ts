91c713dde7b49db2b9376c0fb248e3cf4a7ebfef
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-09-05 16:08:53 +0000

    Fix compile under windows

--- src/app/shared/components/credentials-form/credentials-form.module.ts
+++ src/app/shared/components/credentials-form/credentials-form.module.ts
@@ -1,2 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@ang
+import { CommonModule } from '@angular/common';
+import { CrendetialsFormComponent } from './credentials-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-
