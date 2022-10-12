4922108174f1b9363623a5733da573f706e9a710
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-10-12 17:52:48 +0000

    add logo svg

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,4 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { NgSuspenseModule } from 'ng-suspense';
 
-import { FlexLayoutModule, BREAKPOINTS, DEFAULT
+import { FlexLayoutModule, BREAKPOINTS, DEFAULT_BREAKPOINTS } from '@angular/flex-layout';
+
+import { FormsModule } from '@angular/forms';
+import {
+  ContentSectionModule,
+  CredentialsFormModule,
+  CopyableTextModule,
+  Loa
