78d2cb6279ca0332e9df0536ef0b9b04ac74d2db
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-11-08 20:47:43 +0000

    Typo [ci skip]

--- src/app/transfers/transfers.component.ts
+++ src/app/transfers/transfers.component.ts
@@ -6,4 +6,8 @@ import { MakeTransactionModal, WithdrawModal, DepositModal } from '../modals';
 @Component({
   selector: 'lto-transfers',
   templateUrl: './transfers.component.html',
-  st
+  styleUrls: ['./transfers.component.scss']
+})
+export class TransfersComponent implements OnInit {
+  balance$: Observable<any>;
+  transfers$: Observable<LTO.Page<LTO.Transaction>>;
