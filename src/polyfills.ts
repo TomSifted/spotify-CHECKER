fea804fea7673b5e9c13bd5e15cb9af8dd75d09f
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-11-26 18:00:58 +0000

    Removed useless stuff

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -8,4 +8,7 @@
  *      file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselve
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ *
+ * Learn more in https://angular.io/guide/browser-suppor
