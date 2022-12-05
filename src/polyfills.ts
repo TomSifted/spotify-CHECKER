7d86dd84d6412e50c4561c8f59faa6c407d7ac91
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-12-05 20:21:15 +0000

    sync with code

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -23,4 +23,11 @@
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
 // import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-floa
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'co
