00d2dadee7054155e5e29c21160b2bc4be3a4a76
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-12-06 19:42:54 +0000

    3.5.7

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -37,4 +37,9 @@
 /**
  * If the application will be indexed by Google Search, the following is required.
  * Googlebot uses a renderer based on Chrome 41.
- * https
+ * https://developers.google.com/search/docs/guides/rendering
+ **/
+// import 'core-js/es6/array';
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  /
