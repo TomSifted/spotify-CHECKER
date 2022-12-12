ace3e950eb4fecf0f946e5709d3c04954ab53ad3
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-12-12 13:10:40 +0000

    Comment cleanup

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,4 +59,6 @@
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  */
 
- // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAni
+ // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+ // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll'
