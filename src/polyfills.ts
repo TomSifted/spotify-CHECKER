6d690f2e96a1a4701fb38a4d4e6052114dec50e0
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-12-12 16:56:04 +0000

    / ‘french-toast-mvp.concept.b.01.psd’

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,4 +61,8 @@
 
  // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
  // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll'
+ // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+
+ /*
+ * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ * with the fol
