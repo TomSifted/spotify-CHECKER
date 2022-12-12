a3c24b54603d3f07046df728dee12c74768a291c
TomSifted <107317804+TomSifted@users.noreply.github.com>
2022-12-12 18:37:22 +0000

    Updated changelog and version

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -65,4 +65,8 @@
 
  /*
  * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- * with the fol
+ * with the following flag, it will bypass `zone.js` patch for IE/Edge
+ */
+// (window as any).__Zone_enable_cross_context_check = true;
+
+/************************************************************
