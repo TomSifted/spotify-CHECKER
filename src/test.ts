4198bb23dc2d6872e9ffb1f099faaac77dcaebc8
TomSifted <107317804+TomSifted@users.noreply.github.com>
2023-01-02 17:57:16 +0000

    add meta tags

--- src/test.ts
+++ src/test.ts
@@ -4,4 +4,10 @@ import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDyna
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: any;
+
+// First, initialize the Angular testing environment.
+getTestBed().
